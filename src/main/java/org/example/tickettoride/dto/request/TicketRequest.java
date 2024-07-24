c95737e1c42acee169bfe64c7a16333725896988
rbuchynska <buchynska.ruslana@gmail.com>
2024-07-24 17:19:11 +0300

    Add documentation for TicketRequest

--- src/main/java/org/example/tickettoride/dto/request/TicketRequest.java
+++ src/main/java/org/example/tickettoride/dto/request/TicketRequest.java
@@ -2,6 +2,13 @@ package org.example.tickettoride.dto.request;
 
 import lombok.Data;
 
+/**
+ * The TicketRequest class represents a request to create and save a ticket.
+ * It contains the necessary information for ticket creation, such as departure and arrival points,
+ * number of segments, price, and currency.
+ * Despite task requirements, the travellerAmount field is commented out because the application
+ * uses authorization and retrieves the traveller's balance from the user object.
+ */
 @Data
 public class TicketRequest {
     private String departure;
@@ -9,6 +16,6 @@ public class TicketRequest {
     private int segments;
     private int price;
     private String currency;
-//    private int travellerAmount;
+    // private int travellerAmount;
     private String traveller;
 }
